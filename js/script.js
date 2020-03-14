26de2b19ae095fff6121b717e0878a38df73d7f5
KananValizada <kenan.veli@yahoo.com>
2020-03-14 11:21:54 -0700

    change on CVedit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,48 @@
 
-var nametxt = document.getElementById("aba")
-var names = document.getElementById("person-name")
+var nametxt = document.getElementById("CV-main-header-name")
+var names = document.getElementById("edit-name")
+var city = document.getElementById("edit-city")
+var mobile = document.getElementById("edit-mobile")
+var phone = document.getElementById("edit-phone")
+var mail = document.getElementById("edit-mail")
+var dispcity = document.getElementById("display-city")
+var dispmobile = document.getElementById("display-mobile")
+var dispphone = document.getElementById("display-phone")
+var dispmail = document.getElementById("display-mail")
+var editpict = document.getElementById("edit-pict")
+var editPersonBtn = document.getElementById("edit-btn")
+var disppict =document.getElementById("display-pict")
+editpict.addEventListener("change",function(){
+    var reader = new FileReader();
+    reader.onload = function(e){
+        disppict.src=e.target.result
+        disppict.style.cssText = ""
+    }
+    reader.readAsDataURL(editpict.files[0])
+
+})
+
 
 names.addEventListener("input",function(){
-    console.log(nametxt)
-    names.innerHTML = nametxt.innerHTML
+    nametxt.innerText = names.value
+})
+city.addEventListener("input",function(){
+    dispcity.innerText = city.value
+})
+phone.addEventListener("input",function(){
+    dispphone.innerText = phone.value
+})
+mobile.addEventListener("input",function(){
+    dispmobile.innerText = mobile.value
+})
+mail.addEventListener("input",function(){
+    dispmail.innerText = mail.value
+})
+
+var CvPersonal =document.getElementById("CV-edit-personal")
+var CvIntersts =document.getElementById("CV-edit-interests")
+CvIntersts.style.display="none";
+editPersonBtn.addEventListener("click",function(){
+CvPersonal.style.display="none";
+CvIntersts.style.display="flex";
 })
